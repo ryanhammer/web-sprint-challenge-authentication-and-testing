940c072040fc82432af0417d637db40035994e4c
rhammer35 <rhammer35@icloud.com>
2021-05-21 16:19:23 -0400

    attempted bug fix for restricted middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -2,18 +2,20 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../auth/secrets');
 
 module.exports = (req, res, next) => {
+  console.log('hello from restricted');
   const token = req.headers.authorization;
   if (token) {
     jwt.verify(token, JWT_SECRET, (err, decoded) => {
       if (err) {
-        next({ status: 401, message: 'token invalid' });
+        res.status(401).json({ message: 'token invalid' });
       } else {
         req.decodedJwt = decoded;
         next();
       }
     })
   } else {
-    next({ status: 401, message: 'token required'});
+    console.log('hello from sad path');
+    res.status(401).json({ message: 'token required' });
   }
 };
 
